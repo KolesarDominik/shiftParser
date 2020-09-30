e222b7a0e73ef573a596a3b9e2532f688640487e
dominikkolesar <dominik.kolesar@gmail.com>
2020-09-30 14:10:25 -0700

    refactor

--- script.js
+++ script.js
@@ -1,75 +1,71 @@
-document.getElementsByTagName('button')[0].onclick = (e) => {
-    // text = document.getElementsByTagName('textarea')[0].value
-    
-    var text = document.getElementsByTagName('textarea')[0].value
-    var example = document.getElementById('example')
 
-    var username;
-    var durations;
-    var times;
+let user, dur, tms, total;
+let text, example;
 
-    text.split('--------------------------------').forEach((v)=>{
 
-        // catch username
-        if (v.indexOf('Username: ') > -1 && v.indexOf('=') === -1){
-            username = v.split('Username:')[1].trim()
-        }
+const clearextraction = () => document.getElementById('example').innerHTML = '';
 
-        // catch durations and times
-        if (v.indexOf('Duration: ') > -1 && v.indexOf('=') > -1){
-            durations = [...v.matchAll('[0-9]{1,2}h:[0-9]{1,2}m:[0-9]{1,2}s')].map((a) => a[0].split('').filter(p=>p!=='s' && p!=='h' && p!=='m').join(''))
-            times = [...v.matchAll('[0-9]{1,2} [0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2}')]
-        }
+const parseusername = (tb) => {
 
-        // catch total moment
-        if (v.indexOf('Total Duration') > -1){
-
-            var days = {}
-
-            times.forEach((v,i)=>{
-                if(!days[v[0].split(' ')[0]]){
-                    days[v[0].split(' ')[0]] = []
-                }
-                days[v[0].split(' ')[0]].push(durations[i])
-            })
-
-            
-            example.innerHTML += '<br> ' + classUsername(turnBold(username))
-            Object.keys(days).forEach((key,i)=>{
-                example.innerHTML += '<br> ' + turnBold(key)
-                days[key].forEach((elapsed)=>{
-                    example.innerHTML += '<br> ' + elapsed
-                })
-                example.innerHTML += '<br> ' + '-----------'
-                example.innerHTML += '<br> ' + turnBold(formatTime(days[key].map(timestrToSec).reduce((a,b)=>a+b)))
-                example.innerHTML += '<br>'
-            })
-
-        }
-
-        
-
-    })
-    
-
+    return (tb.indexOf('Username: ') > -1 && tb.indexOf('=') === -1)
+    ? tb.split('Username:')[1].trim()
+    : user;
 }
 
-function classUsername(str){
+const parsedurations = (tb) => {
+
+    return (tb.indexOf('Duration:') > -1 && tb.indexOf('=') > -1)
+    ? [...tb.matchAll('[0-9]{1,2}h:[0-9]{1,2}m:[0-9]{1,2}s')].map((a) => a[0].split('').filter(p=>p!=='s' && p!=='h' && p!=='m').join(''))
+    : dur;
+}
+
+const parsetimes = (tb) => {
+
+    return (tb.indexOf('Duration:') > -1 && tb.indexOf('=') > -1)
+    ? [...tb.matchAll('[0-9]{1,2} [0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2}')]
+    : tms;
+}
+
+const parsetotal = (tb) => {
+
+    if (tb.indexOf('Total Duration') > -1){
+        var days = {}
+
+        tms.forEach((v,i)=>{
+            if(!days[v[0].split(' ')[0]]){
+                days[v[0].split(' ')[0]] = []
+            }
+            days[v[0].split(' ')[0]].push(dur[i])
+        })
+
+        return days;
+    }
+    return total;
+}
+
+const parsetextblock = (tb) => {
+    user = parseusername(tb);
+    dur = parsedurations(tb);
+    tms = parsetimes(tb);
+    total = parsetotal(tb, tms, dur);
+}
+
+const classUsername = (str) => {
     return '<span class="username">' + str + '</span>'
 }
 
-function turnBold(str){
+const turnBold = (str) => {
     return '<b>'+str+'</b>'
 }
 
-function timestrToSec(timestr) {
+const timestrToSec = (timestr) => {
     var parts = timestr.split(":");
     return (parts[0] * 3600) +
            (parts[1] * 60) +
            (+parts[2]);
   }
   
-  function pad(num) {
+  const pad = (num) => {
     if(num < 10) {
       return "0" + num;
     } else {
@@ -77,9 +73,55 @@ function timestrToSec(timestr) {
     }
   }
   
-  function formatTime(seconds) {
+  const formatTime = (seconds) => {
     return [pad(Math.floor(seconds/3600)),
             pad(Math.floor(seconds/60)%60),
             pad(seconds%60),
             ].join(":");
   }
+
+const displayrecord = () => {
+
+    if(total && user){
+
+        example.innerHTML += '<br> ' + classUsername(turnBold(user))
+        Object.keys(total).forEach((key,i)=>{
+            example.innerHTML += '<br> ' + turnBold(key)
+            total[key].forEach((elapsed)=>{
+                example.innerHTML += '<br> ' + elapsed
+            })
+            example.innerHTML += '<br> ' + '-----------'
+            example.innerHTML += '<br> ' + turnBold(formatTime(total[key].map(timestrToSec).reduce((a,b)=>a+b)))
+            example.innerHTML += '<br>'
+        })
+
+
+
+        user = null;
+        dur = null;
+        tms = null;
+        total = null;
+    }
+}
+
+processtextblock = (tb) => {
+
+    parsetextblock(tb);
+    displayrecord();
+
+}
+
+const process = () => {
+
+    example = document.getElementById('example')
+    text = document.getElementsByTagName('textarea')[0].value
+    
+    const textblocks = text.split('--------------------------------');
+
+    textblocks.forEach(processtextblock);
+}
+
+document.getElementsByTagName('button')[1].onclick = (e) => clearextraction();
+
+document.getElementsByTagName('button')[0].onclick = (e) => process();
+
